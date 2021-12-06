5750443d58c8268d695e3e8511444b0083e2b7fe
Abraham E. Tavarez <abeincwireless@gmail.com>
2021-12-05 22:34:17 -0500

    accounting and policies reducers

--- HelloRedux/reducers.js
+++ HelloRedux/reducers.js
@@ -1,5 +1,5 @@
-// Reducers
-// First argument is the current data opr state of our app.
+// =========== Reducers
+// First argument is the current data stored in the state of our app. if any!
 // Second argument is the action object that contains the action.type and payload.
 
 const claimsHistory = (oldListOFClaims = [], action) {
@@ -9,3 +9,20 @@ const claimsHistory = (oldListOFClaims = [], action) {
     };
     return oldListOFClaims;
 };
+
+const accounting = (balance = 100, action) => {
+    if (action.type === "CREATE_CLAIM") {
+        // if it's a claim we deduct the money
+        return balance - action.payload.amountToCollect;
+    } else if (action.type === "CREATE_POLICY") {
+        // if is a new policy we add the money
+        return balance + action.payload.amount;
+    }
+    return balance;
+};
+
+const policies = (listOfPolicies = [], action) => {
+    if (action.type === "CREATE_POLICY") {
+        return [...listOfPolicies, action.payload.name]
+    }
+}
