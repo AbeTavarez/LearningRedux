5477e66dd03a059e8c4924abbd690c5e07f907f3
Abraham E. Tavarez <abeincwireless@gmail.com>
2021-12-08 10:06:12 -0500

    reducers

--- HelloRedux/reducers.js
+++ HelloRedux/reducers.js
@@ -2,7 +2,7 @@
 // First argument is the current data stored in the state of our app. if any!
 // Second argument is the action object that contains the action.type and payload.
 
-const claimsHistory = (oldListOFClaims = [], action) {
+export const claimsHistory = (oldListOFClaims = [], action) => {
     if (action.type === "CREATE_CLAIM") {
         // we create a new array with the new piece of data.
         return [...oldListOFClaims, action.payload]
@@ -10,7 +10,7 @@ const claimsHistory = (oldListOFClaims = [], action) {
     return oldListOFClaims;
 };
 
-const accounting = (balance = 100, action) => {
+export const accounting = (balance = 100, action) => {
     if (action.type === "CREATE_CLAIM") {
         // if it's a claim we deduct the money
         return balance - action.payload.amountToCollect;
@@ -21,8 +21,11 @@ const accounting = (balance = 100, action) => {
     return balance;
 };
 
-const policies = (listOfPolicies = [], action) => {
+export const policies = (listOfPolicies = [], action) => {
     if (action.type === "CREATE_POLICY") {
         return [...listOfPolicies, action.payload.name]
+    } else if (action.type === "DELETE_POLICY") {
+        return listOfPolicies.filter(name => name !== action.payload.name)
     }
+    return listOfPolicies;
 }
