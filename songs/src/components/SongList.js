9643468ad5c9dbb4c634f0c8335431bb7d2c7705
Abraham E. Tavarez <abeincwireless@gmail.com>
2021-12-11 17:17:12 -0500

    rendering data to app

--- songs/src/components/SongList.js
+++ songs/src/components/SongList.js
@@ -1,22 +1,32 @@
-import React, {Component} from 'react';
-
+import React, { Component } from "react";
 // To connect to the store we use:
-import { connect } from 'react-redux';
+import { connect } from "react-redux";
 
 class SongList extends Component {
-    render(){
-        console.log(this.props);
-    return <h1>SongList</h1>
+    renderList() {
+        return this.props.songs.map(song => {
+            return (
+                <div className="item" key={song.title}>
+                    <div className="right floated content">
+                        <button className="ui button primary">Select</button>
+                    </div>
+                    <div className="content">{song.title}</div>
+                </div>
+            )
+        })
     }
-};
+  render() {
+    return <div className="ui divided list">{this.renderList()}</div>
+  }
+}
 
 // by convention we call this function mapStateToProps;
 //this function will get call with all of our state
 const mapStateToProps = (state) => {
-    // this state object will be available as props in our components
-    return {
-        songs: state.songs
-    };
+  // this state object will be available as props in our components
+  return {
+    songs: state.songs,
+  };
 };
 
 // connect return a function that we invoke right after calling connect;
