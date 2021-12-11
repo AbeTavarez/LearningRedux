379da5a6e839ff71ca44e9ff7d00977b053a3aca
Abraham E. Tavarez <abeincwireless@gmail.com>
2021-12-11 15:28:31 -0500

    refactored SongList component to class component

--- songs/src/components/SongList.js
+++ songs/src/components/SongList.js
@@ -1,20 +1,23 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 // To connect to the store we use:
 import { connect } from 'react-redux';
 
-const SongList = () => {
+class SongList extends Component {
+    render(){
+        console.log(this.props);
     return <h1>SongList</h1>
+    }
 };
 
 // by convention we call this function mapStateToProps;
 //this function will get call with all of our state
 const mapStateToProps = (state) => {
-    console.log(state);
+    // this state object will be available as props in our components
     return {
         songs: state.songs
     };
-}
+};
 
-// connect return a function that we invoke right after
+// connect return a function that we invoke right after calling connect;
 export default connect(mapStateToProps)(SongList);
