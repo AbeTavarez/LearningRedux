e70cfea6f06b329dc7d7e5b05422d7ddb1e35389
Abraham E. Tavarez <abeincwireless@gmail.com>
2021-12-11 21:56:19 -0500

    redering all details

--- songs/src/components/SongDetail.js
+++ songs/src/components/SongDetail.js
@@ -2,14 +2,27 @@ import React from "react";
 // 1-Import connect
 import { connect } from "react-redux";
 
-const SongDetail = ({props}) => {
-    console.log(props)
-  return <div>Song Detail</div>;
+const SongDetail = ({ song }) => {
+  // console.log(props)
+
+  if (!song) {
+    return <div>Select a song!</div>;
+  }
+  return (
+    <div className="">
+      <h3>Details for:</h3>
+      <p>
+        Title: {song.title}
+        <br/>
+        Duration: {song.duration}
+      </p>
+    </div>
+  );
 };
 
 // 2- create mapStateToProps and return the data we need in this component
 const mapStateToProps = (state) => {
-    // here we need the selectedSong and we assigned it to the song key 
+  // here we need the selectedSong and we assigned it to the song key
   return { song: state.selectedSong };
 };
 
